9cf0747d292ef8efaaff9a714b5f8ee8fcdd8466
cod3kid <djsufail@gmail.com>
2024-11-02 22:35:01 +0530

    File Upload - Part 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -6,4 +6,14 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to Dynamic AES Encryption with blockchain project"}
 
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    content = await file.read()
+    print("hello",content)
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "size": len(content)
+    }
 
+# While uploading a file, Make sure the key is same as the function parameter, ie, 'file'
